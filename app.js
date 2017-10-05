f4b2562ba8c8cdfc77742bc48d38914020d60f4e
Glenn Goossens <glenngoossens07@gmail.com>
2017-10-05 19:14:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+console.log("app started!");
+
+app.get("/",function(req,res){
+  res.send("Hi there!");
+});
+
+app.get("/bye",function(req,res){
+  res.send("Goodbye!!");
+});
+
+app.get("/dog",function(req,res){
+  res.send("MEOW!!")
+});
+
+app.get("*",function(req,res){
+  res.send("404 error not found");
+});
+
+app.listen(3000,function(){
+  console.log("app running on port 3000");
+});
